39259b0419b7e4c042925fe141bedc8028a30112
Cameron <sicklecj@gmail.com>
2022-03-13 15:26:32 -0500

    more testing

--- develop/script.js
+++ develop/script.js
@@ -21,29 +21,46 @@
 // generateBtn.addEventListener("click", writePassword);
 
 
-console.log("Hello World!");
+// console.log("Hello World!");
 
 
 
-var characterSelect = function () {
+// var characterSelect = function () {
 
-    var characterResult = window.prompt("How many characters long do you want your password to be? Please type a number greater than 8 but less than 128");
+//     var characterResult = window.prompt("How many characters long do you want your password to be? Please type a number greater than 8 but less than 128");
 
-    if (characterResult >= 8 && characterResult <= 128) {
-        console.log("You chose a valid number");
-        window.alert("you chose a valid number");
-    } else {
-        console.log("you chose an invalid option, try again");
-        window.alert("you chose an invalid option, try again");
-        characterSelect();
+//     if (characterResult >= 8 && characterResult <= 128) {
+//         console.log("You chose a valid number");
+//         window.alert("you chose a valid number");
+//     } else {
+//         console.log("you chose an invalid option, try again");
+//         window.alert("you chose an invalid option, try again");
+//         characterSelect();
+//     }
+// }
+
+
+// characterSelect();
+
+
+// console.log("we made it!");
+
+
+
+function runitall() {
+
+    var x = window.prompt("pick a number");
+    for (i = 0; i > -1; i++) {
+        if (x <= 128 && x >= 8) {
+            window.alert("nice number selection");
+            break;
+        } else {
+            window.alert("You choose an invalid number, try again");
+            runitall();
+        }
     }
 }
 
+runitall();
 
-characterSelect();
-
-
-console.log("we made it!");
-
-
-
+console.log("nice we got to it");
