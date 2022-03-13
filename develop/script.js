c56ea0d76eace393f983f729f27364676565fcbc
Cameron <sicklecj@gmail.com>
2022-03-12 18:13:00 -0600

    practice

--- develop/script.js
+++ develop/script.js
@@ -2,17 +2,48 @@
 // TEST COMMENT HERE TO INSURE BRANCHES ARE MERGING AND PUSHING FROM FEATURE TO MAIN CORRECTLY AFTER INITIAL SETUP!
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
 
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//     var password = generatePassword();
+//     var passwordText = document.querySelector("#password");
+
+//     passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+console.log("Hello World!");
+
+
+
+var characterSelect = function () {
+
+    var characterResult = window.prompt("How many characters long do you want your password to be? Please type a number greater than 8 but less than 128");
+
+    if (characterResult >= 8 && characterResult <= 128) {
+        console.log("You chose a valid number");
+        window.alert("you chose a valid number");
+    } else {
+        console.log("you chose an invalid option, try again");
+        window.alert("you chose an invalid option, try again");
+        characterSelect();
+    }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+characterSelect();
+
+
+console.log("we made it!");
+
+
+
