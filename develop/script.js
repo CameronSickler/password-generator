b1c40c6a43a526196af063c907380bd040a44d83
Cameron <sicklecj@gmail.com>
2022-03-13 22:46:08 -0500

    add numeric selection function

--- develop/script.js
+++ develop/script.js
@@ -81,16 +81,16 @@ var upperCaseResult = 0;
 
 function upperCaseSelect() {
     for (i = 0; i > -1; i++) {
-        if (upperCaseResult === "yes") {
+        if (upperCaseResult === "yes" || upperCaseResult === "Yes") {
             console.log("user accepted uppercase characters");
             break;
-        } else if (upperCaseResult === "no") {
+        } else if (upperCaseResult === "no" || upperCaseResult === "No") {
             console.log("user rejected uppercase characters");
             break;
         } else if (i == 0) {
-            upperCaseResult = window.prompt("Do you want to use uppercase character? type yes or no")
+            upperCaseResult = window.prompt("Do you want to use uppercase characters? type yes or no")
         } else {
-            upperCaseResult = window.prompt("You choose an invalid number, try again. Please type yes or no");
+            upperCaseResult = window.prompt("You typed an invalid entry. Please try again. Please type yes or no");
         }
     }
 }
@@ -102,5 +102,65 @@ console.log("we made it past upperCaseSelect");
 console.log(characterResult + " rechecking if characterResult variable still saved");
 console.log(upperCaseResult + " checking if uppercase selection is saved here");
 
-// end data collection for user selection of uppercase character
+// end data collection for user selection of uppercase characters
+
+// begin data collection for user selection of lowercase characters
+
+var lowerCaseResult = 0;
+
+function lowerCaseSelect() {
+    for (i = 0; i > -1; i++) {
+        if (lowerCaseResult === "yes" || lowerCaseResult === "Yes") {
+            console.log("user accepted lowercase characters");
+            break;
+        } else if (lowerCaseResult === "no" || lowerCaseResult === "No") {
+            console.log("user rejected lowercase characters");
+            break;
+        } else if (i == 0) {
+            lowerCaseResult = window.prompt("Do you want to use lowercase characters? type yes or no")
+        } else {
+            lowerCaseResult = window.prompt("You choose an invalid number, try again. Please type yes or no");
+        }
+    }
+}
+
+// call function
+lowerCaseSelect();
+
+console.log("we made it past lowerCaseSelect");
+console.log(characterResult + " rechecking if characterResult variable still saved");
+console.log(upperCaseResult + " rechecking if upperCaseResult variable still saved");
+console.log(lowerCaseResult + " checking if uppercase selection is saved here");
+
+// end data collection for user selection of lowercase characters
+
+// begin data collection for user selection of numeric values
+
+var numericResult = 0;
+
+function numericSelect() {
+    for (i = 0; i > -1; i++) {
+        if (numericResult === "yes" || numericResult === "Yes") {
+            console.log("user accepted lowercase characters");
+            break;
+        } else if (numericResult === "no" || numericResult === "No") {
+            console.log("user rejected lowercase characters");
+            break;
+        } else if (i == 0) {
+            numericResult = window.prompt("Do you want to use numeric characters? type yes or no")
+        } else {
+            numericResult = window.prompt("You choose an invalid option, try again. Please type yes or no");
+        }
+    }
+}
+
+// call function
+numericSelect();
+console.log("we made it past numericSelect");
+console.log(characterResult + " rechecking if characterResult variable still saved");
+console.log(upperCaseResult + " rechecking if upperCaseResult variable still saved");
+console.log(lowerCaseResult + " rechecking if uppercase selection is still saved");
+console.log(numericResult + " checking if numeric selection is saved here");
+
+// end data collection for user selection of numeric values
 
